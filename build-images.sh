b59efa3d2f3152e7c41a2c4d19ad2bf77fe0115e
Ruben Horn <horn.ruben@yahoo.de>
2022-01-25 16:45:16 +0100

    helm: frontend (untested)

--- build-images.sh
+++ build-images.sh
@@ -2,5 +2,5 @@
 
 docker build -t vu_sc_backend ./backend
 docker build -t vu_sc_frontend ./frontend
-# Download MongoDB image
-docker pull mongo
+# Download latest MongoDB image
+# docker pull mongo
