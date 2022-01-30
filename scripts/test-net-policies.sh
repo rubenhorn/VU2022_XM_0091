494f0cdeda62f3e6ed20fed8d6fd7d5bb2850758
Ruben Horn <horn.ruben@yahoo.de>
2022-01-30 18:53:35 +0100

    fixed script

--- scripts/test-net-policies.sh
+++ scripts/test-net-policies.sh
@@ -16,9 +16,9 @@ PODS=$($KUBECTL get pod -n $NAMESPACE | tail -n +2 | awk '{ print $1 }')
 for POD in $PODS; do
     echo - "$POD (egress)"
     if [[ $POD == *"database"* ]]; then
-        ($KUBECTL exec -it -n $NAMESPACE $POD -- sh -c "$CMD_EGRESS_DB $NO_OUTPUT") 2>/dev/null
-        echo - "$POD (ingress)"
-        ($KUBECTL run -n $NAMESPACE -it --rm --restart=Never --image alpine tester -- sh -c "$CMD_INGRESS_DB $NO_OUTPUT") 2>/dev/null
+        ($KUBECTL exec -it -n $NAMESPACE $POD -- sh -c "$CMD_EGRESS_DB $NO_OUTPUT") 2>/dev/null || \
+        echo - "$POD (ingress)" && \
+        ($KUBECTL run -n $NAMESPACE -it --rm --restart=Never --image alpine tester -- sh -c "$CMD_INGRESS_DB $NO_OUTPUT") 1>/dev/null 2>/dev/null
     else
         ($KUBECTL exec -it -n $NAMESPACE $POD -- sh -c "$CMD_EGRESS_FE_BE $NO_OUTPUT") 2>/dev/null
     fi
