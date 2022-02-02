9e46fdb05e3b2933b1d538d477609ef2cdf057c0
Ruben Horn <horn.ruben@yahoo.de>
2022-02-02 19:53:47 +0100

    updated script

--- scripts/gke-nginx-ingress-ctrl.sh
+++ scripts/gke-nginx-ingress-ctrl.sh
@@ -13,20 +13,21 @@ if [ "$1" == down ]; then
 fi
 
 if [ "$(helm list | grep $APP_NAME)" != "" ]; then
-    echo Already installed!
-    exit
+    echo "Already installed!"
+else
+    helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+    helm repo update
+
+    echo -n "Installing helm chart..."
+    helm install  $APP_NAME ingress-nginx/ingress-nginx 1>/dev/null
+    echo " Done!"
 fi
 
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-
-echo "Installing helm chart..."
-helm install  $APP_NAME ingress-nginx/ingress-nginx 1>/dev/null
-echo "Successfully installed nginx ingress controller."
-echo "Waiting for external IP..."
+echo -n "Waiting for external IP..."
 IP="<pending>"
 while [ "$IP" == "<pending>" ]; do
     sleep 1
     IP=$(kubectl get services | grep -e "ingress-nginx-controller.*LoadBalancer" | awk '{ print $4 }')
 done
+echo " Done!"
 echo -e "\nPlease run: export APP_HOSTNAME=$IP.nip.io\n"
